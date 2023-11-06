e5b723857eac5e208290a7bf8d9cbea1c3ee0649
Zubair Qazi <zubair.qazi.5b@gmail.com>
2023-11-06 15:55:41 -0800

    Fix spacing syntax

--- dumper.py
+++ dumper.py
@@ -58,7 +58,7 @@ class ClinicalTrialsGovDumper(HTTPDumper):
                 data = requests.get(remoteurl, params=payload, headers=headers)
             except Exception as e:
                 logger.error(f"Encountered error: {e}, retrying (1) more time...")
-                 data = requests.get(remoteurl, params=payload, headers=headers)
+                data = requests.get(remoteurl, params=payload, headers=headers)
                 
             studies = data.json()
 
