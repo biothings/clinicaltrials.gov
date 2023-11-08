b62fbcdfc0ce45cb0e1378cdf5e3ddb73ed2e9fc
Zubair Qazi <zubair.qazi.5b@gmail.com>
2023-11-08 11:22:36 -0800

    Add debug logging

--- uploader.py
+++ uploader.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import json
 
 import biothings, config
@@ -6,7 +7,9 @@ import biothings, config
 biothings.config_for_app(config)
 
 import biothings.hub.dataload.uploader
-from .parse import load_data
+# from .parse import load_data
+
+from biothings.utils.dataload import dict_sweep
 
 
 class ClinicalTrialsGovUploader(biothings.hub.dataload.uploader.BaseSourceUploader):
@@ -23,4 +26,14 @@ class ClinicalTrialsGovUploader(biothings.hub.dataload.uploader.BaseSourceUpload
 
     def load_data(self, data_folder):
         self.logger.info("Loading data from directory: '%s'" % data_folder)
-        return load_data(data_folder)
+        for infile in glob.glob(os.path.join(data_folder,"*.json")):
+            self.logger.info("Opening json from infile: %s" % infile)
+            doc = json.load(open(infile))
+
+            self.logger.info("Outputting json document: %s" % doc)
+
+            studies = doc["studies"]
+
+            for study in studies:
+                yield dict_sweep(study, ['','null', 'N/A', None, [], {}])
+        # return load_data(data_folder)
